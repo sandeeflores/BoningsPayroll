9dda0f60d8a4d926987cf7dd818316dba97c4bfe
sandee flores <deesan@gmail.com>
2015-02-25 13:50:14 +0800

    Payroll by sandee

--- Payroll.vb
+++ Payroll.vb
@@ -1,3 +1,6 @@
 ﻿Public Class Payroll
 
+    Private Sub Payroll_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        'load pay slip form
+    End Sub
 End Class
